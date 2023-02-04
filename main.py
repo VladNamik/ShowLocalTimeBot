a4d05d0d8dcd40814a034a7e1b1f478a8a04175e
Vladyslav Naminas <vladnamik@gmail.com>
2023-02-04 15:10:10 +0200

    Add start and help message

--- main.py
+++ main.py
@@ -15,11 +15,26 @@ bot = Bot(token=config.api_token)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler()
-async def echo(message: types.Message):
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    # TODO: move to string constants
+    await message.reply("Hi! I'm ShowLocalTime bot!\nI'm new here, so please be gentle...")
+
+
+@dp.message_handler(chat_type=[types.ChatType.SUPERGROUP, types.ChatType.GROUP])
+async def echo_time_for_group(message: types.Message):
     current_time = datetime.now().strftime("%H:%M:%S")
     logging.log(level=logging.INFO, msg=message)
-    await message.answer(message.text + "; " + current_time)
+    await message.answer("For groups time is : " + current_time)
+
+
+@dp.message_handler()
+async def answer_private_messages(message: types.Message):
+    # Ignore direct messages for now
+    pass
 
 
 if __name__ == '__main__':
