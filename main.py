5125237e72bee583340a38080fc4ab5ec033ce60
Vladyslav Naminas <vladnamik@gmail.com>
2023-02-05 00:42:54 +0200

    Get time from location

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import logging
 from datetime import datetime
+from timezonefinder import TimezoneFinder
+from pytz import timezone, utc
 
 from aiogram import Bot, Dispatcher, executor, types
 from utils import Config
+
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 CONFIG_FILEPATH = "./config.json"
 
@@ -26,8 +31,7 @@ async def send_welcome(message: types.Message):
 
 @dp.message_handler(commands=['time'])
 async def show_current_time(message: types.Message):
-    current_time = datetime.now().strftime("%H:%M:%S")
-    await message.reply("It's " + current_time)
+    await message.reply("Please share your location first: ", reply_markup=get_keyboard())
 
 
 @dp.message_handler(chat_type=[types.ChatType.SUPERGROUP, types.ChatType.GROUP])
@@ -41,6 +45,24 @@ async def echo_time_for_group(message: types.Message):
 async def answer_private_messages(message: types.Message):
     # Ignore direct messages for now
     pass
+
+
+def get_keyboard():
+    keyboard = types.ReplyKeyboardMarkup()
+    button = types.KeyboardButton("Share Position", request_location=True)
+    keyboard.add(button)
+    return keyboard
+
+
+@dp.message_handler(content_types=['location'])
+async def handle_location(message: types.Message):
+    lat = message.location.latitude
+    lng = message.location.longitude
+    tf = TimezoneFinder()
+    timezone_data = timezone(tf.timezone_at(lng=lng, lat=lat))
+    reply = "latitude:  {}\nlongitude: {}".format(lat, lng) + "; your time is " + \
+            datetime.now(tz=timezone_data).strftime("%H:%M:%S")
+    await message.answer(reply, reply_markup=types.ReplyKeyboardRemove())
 
 
 if __name__ == '__main__':
